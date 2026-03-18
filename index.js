1c3f4a6d473af3edef5095a209ed736f8cb72cf6
Michael Ptáček <mptacek005@gmail.com>
2026-03-18 17:15:37 +0100

    finished assigment

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-const http = require('node:http');
-const fs = require('node:fs');
+    const http = require('node:http');
+    const fs = require('node:fs');
 
-const server = http.createServer((req, res) => {
+    const server = http.createServer((req, res) => {
 
-    if (req.url === '/about') {
-        res.end(fs.readFile('about.html', (err, DataTransfer) => {
-            
-        }));
-    }
-    
+        if (req.url === '/') {
+            fs.readFile('index.html', (err, data) => {
+                res.end(data);
+            });
+        } else if (req.url === '/about') {
+            fs.readFile('about.html', (err, data) => {
+                res.end(data);
+            })
+        } else if (req.url === '/contactme') {
+            fs.readFile('contactme.html', (err, data) => {
+                res.end(data);
+            })
+        } else {
+            fs.readFile('404.html', (err, data) => {
+                res.statusCode = 404;
+                res.end(data);
+            })
+        }
+        
 
-})
-server.listen(8080);
+    })
+    server.listen(8080);
